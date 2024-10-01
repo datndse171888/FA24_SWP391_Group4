4e6998d1786a4a6155d76e06abf9419addfa9ec0
datndse171888 <datndse171888@fpt.edu.vn>
2024-10-01 20:54:53 +0700

    Add file config axios
    01/10 - 20:54
    datndse171888
    add file config axios

---
+++ PawFund/src/config/axios.jsx
@@ -0,0 +1,19 @@
+const baseURL = '';
+
+const config = {
+    baseURL: baseURL,
+};
+
+const api = axios.create(config);
+
+// api.defaults.baseURL = baseURL;
+
+// const handleBefore = (config) => {
+//     const token = localStorage.getItem('token')?.replaceAll('"', '');
+//     config.headers['Authorization'] = `Bearer ${token}`;
+//     return config;
+// };
+
+// api.interceptors.request.use(handleBefore, null);
+
+export default api;
