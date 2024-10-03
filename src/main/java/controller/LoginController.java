0cc3fe40c68f652869e6436734a1889f18271ec8
longnbbse183111 <longnbbse183111@fpt.edu.vn>
2024-10-03 14:55:55 +0700

    update file LoginController.java

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,42 +1,36 @@
 package controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import service.UserService;
 import dto.UserDTO;
 import entity.User;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.UserService;
 
-@Controller
+@RestController
+@RequestMapping("/api/auth") 
 @AllArgsConstructor
-@SessionAttributes("userdto")
 public class LoginController {
-    private UserService userService;
-    @ModelAttribute("userdto")
-    public UserDTO userDTO(){
-        return new UserDTO();
-    }
-    @GetMapping("/login")
-    public String showLoginForm(){
-        return "/login";
-    }
+
+    private final UserService userService;
+
     @PostMapping("/login")
-    public String Login(@ModelAttribute("userdto") UserDTO userDto, Model model){
-        if(userService.checkUserbyEmail(userDto.getEmail())==false){
-            return "redirect:/login?emailwrong";
-        }
-        User user = userService.getUserbyEmail(userDto.getEmail());
-        if(user.getRole().equals("ADMIN")){
-            return  "redirect:/admin_home";
-        }
-        if(userService.checkPasswordUser(userDto.getEmail(),userDto.getPassword())){
-            return "redirect:/home?success";
+    public ResponseEntity<?> login(@RequestBody UserDTO userDto) {
+        if (!userService.checkUserbyEmail(userDto.getEmail())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email không đúng");
         }
 
-        return "redirect:/login?passwordwrong";
+        User user = userService.getUserbyEmail(userDto.getEmail());
+
+        if ("ADMIN".equals(user.getRole())) {
+            return ResponseEntity.ok("Admin đăng nhập thành công");
+        }
+
+        if (!userService.checkPasswordUser(userDto.getEmail(), userDto.getPassword())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Mật khẩu không đúng");
+        }
+
+        return ResponseEntity.ok("User login successful");
     }
 }
