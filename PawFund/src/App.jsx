c85eb43d75d761392d61112bfea4d8d015a53433
Authur0912 <trinhtbdse171941@fpt.edu.vn>
2024-10-04 18:33:29 +0700

    Register page
    04/10 - 18:22
    trinhtbdse171941
    render register page

--- PawFund/src/App.jsx
+++ PawFund/src/App.jsx
@@ -1,20 +1,21 @@
-import './App.css'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/authentication/login/login.jsx'
-import Home from './pages/userUI/homepage/homepage.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/authentication/login/Login.jsx";
+import Home from "./pages/userUI/homepage/homepage.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./pages/authentication/register/Register.jsx";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- PawFund/src/pages/authentication/login/Login.jsx
+++ PawFund/src/pages/authentication/login/Login.jsx
@@ -1,72 +1,88 @@
-import React from 'react'
-import { useState } from 'react'
-import api from '../../../config/axios.jsx'
-import './login.css'
+// import React from 'react'
+import { useState } from "react";
+import api from "../../../config/axios.jsx";
+import "./login.css";
 
 const Login = () => {
+  const [account, setAccount] = useState({
+    gmail: "",
+    password: "",
+  });
 
-    const [account, setAccount] = useState({
-        gmail: '',
-        password: ''
-    })
+  const handleAccountChange = (e) => {
+    const name = e.target.name;
+    let value = e.target.value;
+    setAccount({
+      ...account,
+      [name]: value,
+    });
+  };
 
-    const handleAccountChange = (e) => {
-        const name = e.target.name;
-        let value = e.target.value;
-        setAccount({
-            ...account,
-            [name]: value
-        })
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      // const json = {
+      //     "email": account.gmail,
+      //     "password": account.password
+      // }
+
+      let formData = new FormData();
+      formData.append("email", account.gmail);
+      formData.append("password", account.password);
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      let res = await api.post("/auth/login", formData, config);
+      const data = res?.data;
+      if (data && data.token) {
+        console.log(data.token);
+        localStorage.setItem("token", data.token);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-
-        try {
-            // const json = {
-            //     "email": account.gmail,
-            //     "password": account.password
-            // }
-
-            let formData = new FormData();
-            formData.append('email', account.gmail);
-            formData.append('password', account.password);
-
-            const config = {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-
-            let res = await api.post('/auth/login', formData, config);
-            const data = res?.data;
-            if (data && data.token) {
-                console.log(data.token);
-                localStorage.setItem('token', data.token);
-            }
-
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    return (
-        <div className="container">
-            <div className="input-form">
-                <div className="input-field">
-                    <input className='input-field_input' type="text" id='gmail' name='gmail' value={account.gmail} onChange={handleAccountChange} required />
-                    <label htmlFor='gmail' className='input-field_label'>Gmail </label>
-                </div>
-                <div className="input-field">
-                    <input className='input-field_input' type="password" id='password' name='password' value={account.password} onChange={handleAccountChange} required />
-                    <label className='input-field_label'>Password </label>
-                </div>
-                <div className="input-button">
-                    <button onClick={handleLogin}>Submit</button>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="input-form">
+        <div className="input-field">
+          <input
+            className="input-field_input"
+            type="text"
+            id="gmail"
+            name="gmail"
+            value={account.gmail}
+            onChange={handleAccountChange}
+            required
+          />
+          <label htmlFor="gmail" className="input-field_label">
+            Gmail{" "}
+          </label>
         </div>
-    )
-}
+        <div className="input-field">
+          <input
+            className="input-field_input"
+            type="password"
+            id="password"
+            name="password"
+            value={account.password}
+            onChange={handleAccountChange}
+            required
+          />
+          <label className="input-field_label">Password </label>
+        </div>
+        <div className="input-button">
+          <button onClick={handleLogin}>Submit</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
