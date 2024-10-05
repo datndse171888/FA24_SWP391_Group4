7a79dfda2fbf937c60022cc08130421599ee0bbc
Authur0912 <trinhtbdse171941@fpt.edu.vn>
2024-10-06 01:35:04 +0700

    Part of body
    06/10
    1:34
    trinhtbdse171941 - some part of  body's homepage

---
+++ PawFund/src/component/body/body.jsx
@@ -0,0 +1,31 @@
+import "./body.css";
+
+const body = () => {
+  return (
+    <div className="body-part">
+      <div className="description-page">
+        <h3 className="title">PAWFUND ADOPTED</h3>
+        <p className="description">
+          The Houston Humane Society is dedicated to, & working towards, ending
+          cruelty, abuse & the overpopulation of animals while providing the
+          highest quality of life to those brought to us for care.
+        </p>
+      </div>
+
+      <div className="help-section">
+        <div className="overlay"></div>
+        <div className="content">
+          <h3 className="title-2">How You Can Help</h3>
+          <div className="button-container">
+            <button className="action-button">DONATE NOW</button>
+            <button className="action-button">DESIGNATE GIFT</button>
+            <button className="action-button">WISH LIST</button>
+            <button className="action-button">TRIBUTE GIFT</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default body;
