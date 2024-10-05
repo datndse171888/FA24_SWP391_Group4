8c06c897fc68d461836a2fcf415d6c3b3ba17378
longnbbse183111 <longnbbse183111@fpt.edu.vn>
2024-10-06 00:46:30 +0700

    update lỗi api

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,48 +1,38 @@
 package controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import service.UserService;
 import dto.UserDTO;
 import entity.User;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 
-<<<<<<< HEAD
 @RestController
 @RequestMapping("/api/auth")
-=======
-@RequestMapping("/api/auth") 
-@RestController
->>>>>>> 4160a34f70f3d1a20c3a50876bccb7f125a9ce79
 @AllArgsConstructor
-@SessionAttributes("userdto")
 public class LoginController {
-    private UserService userService;
-    @ModelAttribute("userdto")
-    public UserDTO userDTO(){
-        return new UserDTO();
-    }
-    @GetMapping("/login")
-    public String showLoginForm(){
-        return "/login";
-    }
+
+    private final UserService userService;
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserDTO userDto) {
+        // Kiểm tra email có tồn tại không
         if (!userService.checkUserbyEmail(userDto.getEmail())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email không đúng");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Thông tin đăng nhập không hợp lệ");
         }
 
+        // Lấy thông tin người dùng
         User user = userService.getUserbyEmail(userDto.getEmail());
 
+        // Kiểm tra vai trò
         if ("ADMIN".equals(user.getRole())) {
             return ResponseEntity.ok("Admin đăng nhập thành công");
         }
 
+        // Kiểm tra mật khẩu
         if (!userService.checkPasswordUser(userDto.getEmail(), userDto.getPassword())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Mật khẩu không đúng");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Thông tin đăng nhập không hợp lệ");
         }
 
         return ResponseEntity.ok("User login successful");
