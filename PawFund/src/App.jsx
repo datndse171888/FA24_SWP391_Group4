c3d5299cb7b8f7caba6331a026517305d4aae7fa
datndse171888 <datndse171888@fpt.edu.vn>
2024-10-06 00:19:15 +0700

    Header
    06/10 - 00:18
    datndse171888
    basic header for website

--- PawFund/src/App.jsx
+++ PawFund/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/authentication/login/login.jsx'
 import Home from './pages/userUI/homepage/homepage.jsx'
+import PetList from './pages/userUI/pet/viewPetList.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -9,9 +10,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Navigate to="home" />} />
+        <Route path="home" element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route path="petlist" element={<PetList />} />
       </Routes>
     </BrowserRouter>
   )
--- PawFund/src/pages/userUI/homepage/homepage.jsx
+++ PawFund/src/pages/userUI/homepage/homepage.jsx
@@ -3,17 +3,12 @@ import Header from '../../../component/header/header'
 import Card from '../../../component/card/card'
 
 const homepage = () => {
-  const json = {
-    
-    "name": "Kitty",
-    "age": "Medium",
-    "breed": "Siamese"
-  }
+  
   
   return (
     <div>
       <Header />
-      <Card cat={json} />
+      
     </div>
   )
 }
