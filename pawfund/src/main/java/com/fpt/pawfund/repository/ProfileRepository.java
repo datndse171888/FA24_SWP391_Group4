eda3208ca8936c51d630bb9aed071e40697127ac
Nguyen Tuan Kiet <kietntse183069@fpt.edu.vn>
2024-10-14 08:13:47 +0700

    Delete pawfund directory

--- pawfund/src/main/java/com/fpt/pawfund/repository/ProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fpt.pawfund.repository;
-
-
-import com.fpt.pawfund.model.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Integer> {
-}
--- pawfund/src/main/java/com/fpt/pawfund/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fpt.pawfund.repository;
-
-
-import com.fpt.pawfund.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
