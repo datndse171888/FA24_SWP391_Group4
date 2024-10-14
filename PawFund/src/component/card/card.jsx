f5bedc99c7ec635f0a6cdd7bf0353d650316ec7c
datndse171888 <datndse171888@fpt.edu.vn>
2024-10-14 08:12:09 +0700

    Config login
    14/10 - 08:11
    datndse171888
    add route for shelterUI, config api for login, add adopterList

--- PawFund/src/component/card/card.jsx
+++ PawFund/src/component/card/card.jsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 
 
 const card = ({ cat }) => {
-  cat.id = useState(1)
+  const click = () => {
+    console.log(cat.id)
+  }
   return (
-    <div className="col-2 listCat">
+    <div className="col-3">
       <Card >
         <Card.Img className='ImgCatList' src={cat.img} />
         <Card.Body>
@@ -16,7 +18,7 @@ const card = ({ cat }) => {
             <p className="m-0">Breed: {cat.breed}</p>
             <p className="m-0">Gender: {cat.gender}</p>
           </Card.Text>
-          <Button variant="primary" className="btnViewDetail" href="catdetail/${cat.id}" >View detail</Button>
+          <Button variant="primary" className="btnViewDetail" href={`catdetail/${cat.id}`} >View detail</Button>
         </Card.Body>
       </Card>
     </div>
--- PawFund/src/pages/shelterUI/adoptedApplication/adoptedList.jsx
+++ PawFund/src/pages/shelterUI/adoptedApplication/adoptedList.jsx
@@ -1,28 +1,45 @@
 import React from 'react'
+import { useState } from 'react';
+import { Tab, Row, Col, ListGroup } from 'react-bootstrap';
+import Header from '../../../component/header/header.jsx';
+import './adoptedList.css';
 
 const adoptedList = () => {
-const [adoptedInfo, setAdoptedInfo] = useState({
-    
-});
+
+    const [adoptedInfo, setAdoptedInfo] = useState({
+
+    });
+
+    const list = [
+        {
+            id: 1,
+            name: 'Kitty',
+        },
+        {
+            id: 2,
+            name: 'Tommy',
+        }
+    ]
 
     return (
         <div>
-            <Tab.Container id="list-group-tabs-example">
+            <Header />
+            <Tab.Container id="list-group-tabs-example" className="d-flex">
                 <Row>
                     <Col sm={4}>
                         <ListGroup>
-                            <ListGroup.Item action href="#link1">
-                                Link 1
-                            </ListGroup.Item>
-                            <ListGroup.Item action href="#link2">
-                                Link 2
-                            </ListGroup.Item>
+                            {list.map((item, index) => (
+                                <ListGroup.Item key={index} action href={`#${item.id}`}>
+                                    {item.name}
+                                </ListGroup.Item>
+                            ))}
                         </ListGroup>
                     </Col>
-                    <Col sm={8}>
+                    <Col sm={8} className='border' id='info-content'>
                         <Tab.Content>
-                            <Tab.Pane eventKey="#link1">Tab pane content 1</Tab.Pane>
-                            <Tab.Pane eventKey="#link2">Tab pane content 2</Tab.Pane>
+                            {list.map((item, index) => (
+                                <Tab.Pane key={index} eventKey={`#${item.id}`}>{item.name}</Tab.Pane>
+                            ))}
                         </Tab.Content>
                     </Col>
                 </Row>
