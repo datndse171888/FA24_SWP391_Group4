0d0640918174bcf80f0c1194e840c03ffcec518c
TrangThu5 <thutlmse171876@fpt.edu.vn>
2024-10-17 07:40:55 +0700

    Update render dashboard
    7:39 - 17/10
    Thutlmse171876
    Update render dashboard

--- PawFund/src/pages/adminUI/dashboard/dashboard.jsx
+++ PawFund/src/pages/adminUI/dashboard/dashboard.jsx
@@ -1,15 +1,53 @@
 import React from 'react'
-import { Nav, Navbar } from 'react-bootstrap'
+import { Button, Nav, Navbar } from 'react-bootstrap'
+import './dashboard.css'
 const dashboard = () => {
   return (
-    <div >
-      <Navbar.Collapse className='d-flex justify-content-center'>
-        <Nav className=''>
-          <Nav.Link className='px-5 py-3 nav-link' href="/">User</Nav.Link>
-          <Nav.Link className='px-5 py-3 nav-link' href="/viewlist">Donation</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+    <div className='row dashboardbig' >
+      <div className='col-3 dashboardbar'>
+        <div className='titleDashboard'>
+          <h1>
+            Dashboard
+          </h1>
+        </div>
+        <div>
+          <Nav className=' flex-column'>
+            <Nav.Link className='nav-link bodyDashboard' href="/">User</Nav.Link>
+            <Nav.Link className='nav-link bodyDashboard' href="/">Donation</Nav.Link>
+            <Nav.Link className='nav-link bodyDashboard' href="/">Cat</Nav.Link>
+            <Nav.Link className='nav-link bodyDashboard' href="/">Donor</Nav.Link>
+          </Nav>
+        </div>
+      </div>
+      <div className='col-9 '>
+        <div className='row dashboardMain'>
+          <div className='col-6 col1Dashboard' >
+            <div className='DboardUser'>
+              <h1 className='titleUser'>User</h1>
+              <Button className='btnUser' variant="outline-primary" href="/">User</Button>
+            </div>
+
+            <div className='DboardDonation'>
+              <h1>Donation</h1>
+              <Button variant="outline-primary" href="/">Donation</Button>
+            </div>
+
+          </div>
+          <div className='col-6' >
+            <div className='DboardCat'>
+              <h1>Number of cat</h1>
+              <Button variant="outline-primary" href="/">Cat</Button>
+            </div>
+
+            <div className='DboardDonor'>
+              <h1>Donor</h1>
+              <Button variant="outline-primary" href="/">Donor</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
   )
 }
 
